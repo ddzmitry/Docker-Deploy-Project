90ccadb8941e50764b3923b9ff485e24f05e94d2
Dzmitry Dubarau <dzmitry.dubarau@gmail.com>
2019-04-13 17:59:34 -0400

    added text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello I was changed on the feature branch
+            Hello Anastasia
           </p>
           <a
             className="App-link"
